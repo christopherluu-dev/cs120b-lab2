f42336607db4a7da7faaea9ab1e26a77f5e8b207
Chris <christopherluu.dev@gmail.com>
2021-09-29 12:58:39 -0700

    Initializing repository

---
+++ source/main.c
@@ -0,0 +1,26 @@
+/*	Author: christopherluu
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF; //Configure port A's pins as input
+    DDRB = 0xFF; PORTB = 0x00; //Configure port B's pins as output
+    
+    unsigned char temp_input = 0x00;
+    while (1) {
+        temp_input = PINA;
+        PORTB = temp_input;
+    }
+    return 1;
+}
