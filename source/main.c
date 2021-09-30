035700fe1b236e037fdfdd507427b608a2cfa2d1
Chris <christopherluu.dev@gmail.com>
2021-09-30 11:38:13 -0700

    fixed the headers

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*	Author: christopherluu
  *  Partner(s) Name: 
  *	Lab Section: 22 
- *	Assignment: Lab #2  Exercise #2
+ *	Assignment: Lab #2  Exercise #4
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
