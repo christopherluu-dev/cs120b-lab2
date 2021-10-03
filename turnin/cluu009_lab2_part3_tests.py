6a556d8f3e03481627e2b431f5b57de8c48561c5
Chris <christopherluu.dev@gmail.com>
2021-10-03 15:58:29 -0700

    added the test cases

--- turnin/cluu009_lab2_part3_tests.py
+++ turnin/cluu009_lab2_part3_tests.py
@@ -1,15 +1,19 @@
-tests = [ {'description': 'PINA: 0x00 => PORTB: 0x02',
+tests = [ {'description': 'PINA: 0x00 => PORTC: 0x04',
     'steps': [ {'inputs': [('PINA',0x00)], 'iterations': 5 } ],
-    'expected': [('PORTB',0x02)],
+    'expected': [('PORTC',0x04)],
     },
-    {'description': 'PINA: 0x02 => PORTB: 0x02',
-    'steps': [ {'inputs': [('PINA',0x02)], 'iterations': 5 } ],
-    'expected': [('PORTB',0x02)],
+    {'description': 'PINA: 0x04 => PORTC: 0x03',
+    'steps': [ {'inputs': [('PINA',0x04)], 'iterations': 5 } ],
+    'expected': [('PORTC',0x03)],
     },
-    {'description': 'PINA: 0x01 => PORTB: 0x01',
-    'steps': [ {'inputs': [('PINA',0x01)], 'iterations': 5 } ],
-    'expected': [('PORTB',0x01)],
+    {'description': 'PINA: 0x05 => PORTC: 0x02',
+    'steps': [ {'inputs': [('PINA',0x05)], 'iterations': 5 } ],
+    'expected': [('PORTC',0x02)],
     },
-    ]
+    {'description': 'PINA: 0x0D => PORTC: 0x01',
+    'steps': [ {'inputs': [('PINA',0x0D)], 'iterations': 5 } ],
+    'expected': [('PORTC',0x01)],
+    },
+]
 #watch = ['PORTB']
 
